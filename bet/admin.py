e7d3d192b239b3f400eb82e7cc172c5f2f8b27a2
Your Name <artger@ukr.net>
2020-03-02 21:37:58 +0200

    register bet models in admin.py

--- bet/admin.py
+++ bet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BetCategories, Game, Coefficients, Bets, UserBets
 
-# Register your models here.
+admin.site.register(BetCategories)
+admin.site.register(Game)
+admin.site.register(Coefficients)
+admin.site.register(Bets)
+admin.site.register(UserBets)
